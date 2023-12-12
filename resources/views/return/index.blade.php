a422ad96ab531b7aca888b4c2207ab510bd12167
sws-ava <77443084+sws-ava@users.noreply.github.com>
2023-12-12 10:03:11 +0200

    favicon

--- resources/views/return/index.blade.php
+++ resources/views/return/index.blade.php
@@ -4,7 +4,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Return</title>
+    <title>Возврат товара</title>
+    <link rel="icon" type="image/svg+xml" href="/images/favicon.png" />
     <link href="/css/style.css" rel="stylesheet">
     <link href="/css/print.css" rel="stylesheet">
     <link rel="preconnect" href="https://fonts.googleapis.com">
